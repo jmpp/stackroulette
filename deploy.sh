7da7386c03385d2beb5dd6ec130bfe362bc52f32
J.M. Cléry <jmclery@gmail.com>
2020-04-28 04:16:23 +0200

    Adds deployment config for GH Pages

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# deploying to a custom domain
+# echo 'stackroulette.dev' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# deploying to https://jmpp.github.io/stackroulette
+git push -f git@github.com:jmpp/stackroulette.git master:gh-pages
+
+cd -
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+// vue.config.js
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/stackroulette/' : '/'
+};
